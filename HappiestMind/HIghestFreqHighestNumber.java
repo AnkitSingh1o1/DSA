216ed4b94b159587e6b6918fac033b905a9b5b6e
Ankit Singh <ankitsinghniti@gmail.com>
2023-07-18 23:45:18 +0530

    Commit

---
+++ HappiestMind/HIghestFreqHighestNumber.java
@@ -0,0 +1,39 @@
+package HappiestMind;
+
+import java.io.*;
+import java.util.*;
+
+public class HIghestFreqHighestNumber {
+    public static void main(String[] args) throws IOException{
+
+        //Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] strArray = br.readLine().split(" ");
+        int[] arr = new int[strArray.length];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(strArray[i]);
+        }
+
+        //Logic
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+
+        int maxFreq = Integer.MIN_VALUE, maxKey = Integer.MIN_VALUE;
+        for(var v : map.entrySet()){
+            int key = v.getKey();
+            int val = v.getValue();
+
+            if(val >= maxFreq && key > maxKey){
+                maxFreq = val;
+                maxKey = key;
+            }
+        }
+
+        System.out.println(maxKey);
+
+    }
+}
