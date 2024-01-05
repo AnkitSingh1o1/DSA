7e6099544951df374b465c17740c5a1d4f312b71
Ankit Singh <ankitsinghniti@gmail.com>
2024-01-05 18:41:03 +0530

    05012024

---
+++ Blind75/Binary/Solution_268.java
@@ -0,0 +1,11 @@
+public class Solution_268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n*(n+1))/2;
+        for(int i : nums){
+            sum -= i;
+        }
+
+        return sum;
+    }
+}
